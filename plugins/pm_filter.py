ed7ed36586f71a87299a29721c6f1331a64bc16b
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2025-03-29 13:46:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -161,7 +161,7 @@ async def next_page(bot, query):
                 ]
             )
         else:
-            btn.insert(0, [InlineKeyboardButton("𝐇𝐨𝐰 𝐓𝐨 𝐆𝐞𝐭 𝐅𝐢𝐥𝐞𝐬", url="t.me/MoviezAddaKann/6")])
+            btn.insert(0, InlineKeyboardButton("⚡ 𝐇𝐨𝐰 𝐓𝐨 𝐆𝐞𝐭 𝐅𝐢𝐥𝐞𝐬 ⚡", url=f"https://t.me/MoviezAddaKann/10"))
             btn.insert(1, [
                             InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', callback_data='minfo'),
                             InlineKeyboardButton(f'ꜱᴇʀɪᴇꜱ', callback_data='sinfo')
